19e752e04e2c84962ebbb1984a9c555c92cc042f
Abid0624 <abidkhan09989@gmail.com>
2024-09-20 16:20:46 +0600

    added partially the add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,23 @@
-console.log('connected')
+// add money to the account
+/**
+ * S-1 : add event handler,prevent page reload after form submit
+ * S-2 : get money to be added to the account balance
+ * S-3 : get the pin number
+ * 
+ **/ 
+
+
+
+// Step - 1
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+  // prevent page reload after page reload
+  event.preventDefault();
+
+  // Step - 2
+  const addMoneyInput = document.getElementById('input-add-money').value
+  console.log(addMoneyInput);
+
+  // Step - 3
+  const pinNumber = document.getElementById('pin-number').value
+  console.log(pinNumber);
+})

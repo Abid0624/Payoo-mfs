1bbfff703c4b0ab7b46e9ecf68144467819d14d6
Abid0624 <abidkhan09989@gmail.com>
2024-09-20 15:54:53 +0600

    add the home part

--- js/login.js
+++ js/login.js
@@ -4,10 +4,22 @@ document.getElementById('btn-login').addEventListener('click',function(event){
 
   // Step -2: Prevent default behavior (prevent reloading page)
   event.preventDefault();
-  console.log('login button clicked')
+  // console.log('login button clicked')
 
-  // Step -3:get the phone number
+  // Step -3:get the phone number and pin number
   const phoneNumber = document.getElementById('phone-number').value
-  console.log(phoneNumber)
+  const pinNumber = document.getElementById('pin-number').value
+  // console.log(phoneNumber,pinNumber);
+
+  // this is temporary and you should not do like this;
+
+  // step -4 :validation phone and pin
+  if (phoneNumber === '01799208309' && pinNumber === '1234'){
+    console.log('You are logged in');
+    window.location.href = '/home.html';
+  }
+  else{
+    alert('Wrong phone number or pin');
+  }
 
 })
